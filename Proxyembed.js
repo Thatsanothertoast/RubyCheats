1667962be31cfb383594ee1950e46e090de56635
Thatsanothertoast <toastyt678@gmail.com>
2025-04-06 10:26:59 -0700

    Create Proxyembed.js

---
+++ Proxyembed.js
@@ -0,0 +1,1 @@
+javascript:document.body.innerHTML = "<iframe src=\"https:\/\/sayless.series.3.suarakeadilan.org\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
